15644af54ac34b20a90c3588a1a8881ae56af976
MatildosKiller <lsnkandrey@outlook.com>
2026-02-22 19:16:08 +0500

    buisness logic in DatabaseHelper has completed

---
+++ HostelDataBaseApp/Bookings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelDataBaseApp
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int GuestId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string GuestName { get; set; }
+        public string RoomNumber { get; set; }
+    }
+}
--- HostelDataBaseApp/DataBaseHelper.cs
+++ HostelDataBaseApp/DataBaseHelper.cs
@@ -50,4 +50,137 @@ public static class DatabaseHelper
             command.ExecuteNonQuery();
         }
     }
+
+    public static List<Room> GetRooms()
+    {
+        var rooms = new List<Room>();
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("SELECT * FROM Rooms", connection);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    rooms.Add(new Room
+                    {
+                        RoomId = reader.GetInt32("RoomId"),
+                        RoomNumber = reader.GetString("RoomNumber"),
+                        Capacity = reader.GetInt32("Capacity"),
+                        PricePerNight = reader.GetDecimal("PricePerNight"),
+                        IsActive = reader.GetBoolean("IsActive")
+                    });
+                }
+            }
+        }
+        return rooms;
+    }
+
+
+    // Поиск свободных номеров на период
+    public static List<Room> FindAvailableRooms(DateTime checkIn, DateTime checkOut)
+    {
+        var availableRooms = new List<Room>();
+        //LINQ
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            var sql = @"
+                SELECT r.*
+                FROM Rooms r
+                WHERE r.IsActive = 1
+                AND r.RoomId NOT IN (
+                    SELECT b.RoomId
+                    FROM Bookings b
+                    WHERE b.Status = 'Active'
+                    AND (
+                        (b.CheckInDate <= @CheckOut AND b.CheckOutDate >= @CheckIn)
+                    )
+                )";
+
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@CheckIn", checkIn);
+            command.Parameters.AddWithValue("@CheckOut", checkOut);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    availableRooms.Add(new Room
+                    {
+                        RoomId = reader.GetInt32("RoomId"),
+                        RoomNumber = reader.GetString("RoomNumber"),
+                        Capacity = reader.GetInt32("Capacity"),
+                        PricePerNight = reader.GetDecimal("PricePerNight")
+                    });
+                }
+            }
+        }
+
+        return availableRooms;
+    }
+
+    // Создание бронирования
+    public static int CreateBooking(int guestId, int roomId, DateTime checkIn, DateTime checkOut, decimal totalAmount)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            var sql = @"
+                INSERT INTO Bookings (GuestId, RoomId, CheckInDate, CheckOutDate, TotalAmount)
+                VALUES (@GuestId, @RoomId, @CheckIn, @CheckOut, @TotalAmount);
+                SELECT SCOPE_IDENTITY();";
+
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@GuestId", guestId);
+            command.Parameters.AddWithValue("@RoomId", roomId);
+            command.Parameters.AddWithValue("@CheckIn", checkIn);
+            command.Parameters.AddWithValue("@CheckOut", checkOut);
+            command.Parameters.AddWithValue("@TotalAmount", totalAmount);
+
+            return Convert.ToInt32(command.ExecuteScalar());
+        }
+    }
+
+    // Получение бронирований
+    public static List<Booking> GetBookings()
+    {
+        var bookings = new List<Booking>();
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sql = @"
+                SELECT b.*, g.FullName, r.RoomNumber
+                FROM Bookings b
+                JOIN Guests g ON b.GuestId = g.GuestId
+                JOIN Rooms r ON b.RoomId = r.RoomId";
+
+            var command = new SqlCommand(sql, connection);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    bookings.Add(new Booking
+                    {
+                        BookingId = reader.GetInt32("BookingId"),
+                        GuestId = reader.GetInt32("GuestId"),
+                        RoomId = reader.GetInt32("RoomId"),
+                        CheckInDate = reader.GetDateTime("CheckInDate"),
+                        CheckOutDate = reader.GetDateTime("CheckOutDate"),
+                        TotalAmount = reader.GetDecimal("TotalAmount"),
+                        Status = reader.GetString("Status"),
+                        CreatedDate = reader.GetDateTime("CreatedDate"),
+                        GuestName = reader.GetString("FullName"),
+                        RoomNumber = reader.GetString("RoomNumber")
+                    });
+                }
+            }
+        }
+        return bookings;
+    }
+
+
 }
